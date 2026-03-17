a6768a132644dc7f2b61f7415843cc84aa5f96b0
Isabella Alvarenga <alvxrenga.isa@gmail.com>
2026-03-17 15:55:32 -0300

    Corrigi erros de sintaxe

--- exercicio15.c
+++ exercicio15.c
@@ -2,14 +2,20 @@
 #include <stdio.io>
 #include <math.h>
 
+void addNumbers();
+void findSquare();
+
 int main(){
-	int a,b,op;
+	int a,b,option;
 	printf("---------------\n");
-	printf("CHOOSE AN OPTION\n");
+	printf("MENU\n");
 	printf("1 - Sum\n");
 	printf("2 - Square\n");
 	printf("3 - Exit\n");
 	printf("---------------\n");
+	printf("Choose an option: ");
+	scanf("%d",&option);
+	
 	
 	return 0;
 }
