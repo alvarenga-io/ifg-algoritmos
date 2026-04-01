208833d8213e47cdd18b4e719fe4e1c08282afce
Isabella Alvarenga <alvxrenga.isa@gmail.com>
2026-04-01 11:25:10 -0300

    Reestruturei os exercícios com loop duplo

--- lista-02a/07.c
+++ lista-02a/07.c
@@ -1,33 +1,32 @@
-// EXERCÍCIO 07
+// EXERCÍCIO 2.7
+
 #include <stdio.h>
-#include <stdbool.h>
 
-int main() {
-    int limite, i, j, totalPrimos = 0;
-    bool primo;
+int main(){
+    int primo = 1,num, count = 0;
 
-    printf("Digite um número: ");
-    scanf("%d", &limite);
-
-    printf("\nNúmeros primos no intervalo de 1 a %d:\n", limite);
-
-    for (i = 2; i <= limite; i++) {
-        primo = true;
-
-        for (j = 2; j <= i / 2; j++) {
-            if (i % j == 0) {
-                primo = false;
-                break;
-            }
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    printf("Numeros primos no intervalo de 1 a %d \n", num);
+    printf("-------------------------------------------------\n");
+    for(int i = 2; i<=num; i++){
+        primo = 1;
+        for(int j = 2; j<i; j++){
+            if (i%j == 0){
+                primo = 0; 
+                break; 
+            } 
+            
         }
-
-        if (primo) {
-            printf("%d ", i);
-            totalPrimos++;
+        
+        if(primo == 1){
+            count ++;
+            printf("%d ...", i);
         }
     }
-
-    printf("\n\nQuantidade total de números primos: %d\n", totalPrimos);
+    printf("\n-------------------------------------------------\n");
+    printf("\nContagem de numeros primos no intervalo: %d\n", count);
 
     return 0;
 }
+
--- lista-02a/08.c
+++ lista-02a/08.c
@@ -1,36 +1,33 @@
+// EXERCÍCIO 08
+
 #include <stdio.h>
 
-int main() {
-    int numeros[4];
-    int i, j, aux;
+int main(){
+    int numeros[4], aux, i = 0;
+    printf("Digite 4 numeros: \n");
 
-
-    printf("Digite quatro numeros inteiros:\n");
-    for (i = 0; i < 4; i++) {
-        printf("Numero %d: ", i + 1);
-        scanf("%d", &numeros[i]);
+    for(i = 0; i<4; i++){
+        printf("Numero %d: ", i +1);
+        scanf("%d", & numeros[i] );
     }
 
-    for (i = 0; i < 4; i++) {
-        for (j = i + 1; j < 4; j++) {
-            if (numeros[i] > numeros[j]) {
+    for(i =0; i<4; i++){
+        for(int j = i + 1; j <4; j++){
+            if(numeros[j]>numeros[i]){
                 aux = numeros[i];
                 numeros[i] = numeros[j];
                 numeros[j] = aux;
             }
         }
     }
-
-    printf("\nA) Ordem crescente: ");
-    for (i = 0; i < 4; i++) {
+    printf("\nOrdem crescente\n");
+    for (i =3; i>=0; i--){
         printf("%d ", numeros[i]);
     }
-
-    printf("\nB) Ordem decrescente: ");
-    for (i = 3; i >= 0; i--) {
+    printf("\nOrdem decrescente:\n");
+    for(i=0; i<4; i++){
         printf("%d ", numeros[i]);
     }
-
     printf("\n");
     return 0;
 }
